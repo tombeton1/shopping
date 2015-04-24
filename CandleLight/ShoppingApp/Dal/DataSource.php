d21473fd006d6c9f23c2b9b790345c91527671c6
Tom Adriaens <tomadriaens@yahoo.com>
2015-04-24 21:19:55 +0200

    pass veranderd

--- CandleLight/ShoppingApp/Dal/DataSource.php
+++ CandleLight/ShoppingApp/Dal/DataSource.php
@@ -25,7 +25,7 @@ class DataSource
     {
         $conn = NULL;
         try {
-            $conn = new \PDO("mysql:host=localhost;port=3306;dbname=test", "root", "root", array(\PDO::ATTR_PERSISTENT => true));
+            $conn = new \PDO("mysql:host=localhost;port=3306;dbname=shopping_db", "root", "root", array(\PDO::ATTR_PERSISTENT => true));
             $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
         } catch (\PDOException $e) {
