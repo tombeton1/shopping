7c34398ab0249e7caaee1f729e1f27e151359fd4
Tom Adriaens <tomadriaens@yahoo.com>
2015-04-24 19:05:48 +0200

    git try product

--- CandleLight/ShoppingApp/Bo/Products.php
+++ CandleLight/ShoppingApp/Bo/Products.php
@@ -1,14 +1,104 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: lenny
- * Date: 24/04/15
- * Time: 18:55
+ * User: Tom
+ * Date: 24/04/2015
+ * Time: 16:55
  */
 
 namespace ShoppingApp\Bo;
 
 
-class Products {
+class Product {
+    private $productId;
+    private $productName;
+    private $productCategory;
+    private $productPrice;
+    private $productDescription;
 
-} 
+    function __construct()
+    {
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getProductId()
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param mixed $productId
+     */
+    public function setProductId($productId)
+    {
+        $this->productId = $productId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProductName()
+    {
+        return $this->productName;
+    }
+
+    /**
+     * @param mixed $productName
+     */
+    public function setProductName($productName)
+    {
+        $this->productName = $productName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProductCategory()
+    {
+        return $this->productCategory;
+    }
+
+    /**
+     * @param mixed $productCategory
+     */
+    public function setProductCategory($productCategory)
+    {
+        $this->productCategory = $productCategory;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProductPrice()
+    {
+        return $this->productPrice;
+    }
+
+    /**
+     * @param mixed $productPrice
+     */
+    public function setProductPrice($productPrice)
+    {
+        $this->productPrice = $productPrice;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProductDescription()
+    {
+        return $this->productDescription;
+    }
+
+    /**
+     * @param mixed $productDescription
+     */
+    public function setProductDescription($productDescription)
+    {
+        $this->productDescription = $productDescription;
+    }
+
+}
