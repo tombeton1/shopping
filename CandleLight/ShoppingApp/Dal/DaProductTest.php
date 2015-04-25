f3055f686ed3bf8e23a78b0714ec12de1def4770
Tom Adriaens <tomadriaens@yahoo.com>
2015-04-25 20:11:54 +0200

    Revert "DaProduct selectOne"
    This reverts commit 5ac792720eb0a0a5989bddde6846c9f3af3d45ef.

--- CandleLight/ShoppingApp/Dal/DaProductTest.php
+++ CandleLight/ShoppingApp/Dal/DaProductTest.php
@@ -23,13 +23,10 @@ $product = new Product();
 //$product->setProductId(1);
 //DaProduct::delete($product);
 
-//$product->setProductId('3');
-//$product->setProductCategory('2');
-//$product->setProductName('product Updated');
-//$product->setProductPrice('Super, 60');
-//$product->setProductDescription('blal lalal  alla llallalal');
-//DaProduct::update($product);
+$product->setProductId('3');
+$product->setProductCategory('2');
+$product->setProductName('product Updated');
+$product->setProductPrice('Super, 60');
+$product->setProductDescription('blal lalal  alla llallalal');
+DaProduct::update($product);
 
-$product->setProductId(1);
-DaProduct::selectOne($product);
-echo 'dit is the '.$product->getProductName();
