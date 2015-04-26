66c51574783a3d0979c33462385a94872b44d48e
Tom Adriaens <tomadriaens@yahoo.com>
2015-04-26 19:22:05 +0200

    product select one

--- shoppingDB/storedprocedures/product-select-one.sql
+++ shoppingDB/storedprocedures/product-select-one.sql
@@ -1,11 +1,18 @@
 DROP PROCEDURE IF EXISTS product_select_one;
+
 DELIMITER //
 CREATE PROCEDURE `product_select_one`
+
 (
 	 pId INT 
 )
+
 BEGIN
 SELECT * FROM `product`
-		WHERE `product`.`product_id` = pId;
+	inner join	
+	`product_category` on `product`.`product_id` = `product_category`.`product_category_id`		
+	WHERE `product`.`product_id` = pId;
+
 END //
 DELIMITER ;
+
