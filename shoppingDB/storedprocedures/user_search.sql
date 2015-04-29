1799af674957bccd07b6f4b95437fec75ad66472
Lenny <lenny.donnez@gmail.com>
2015-04-29 15:12:03 +0200

    user_search SP(werkt niet)

---
+++ shoppingDB/storedprocedures/user_search.sql
@@ -0,0 +1,12 @@
+USE shopping_db;
+DROP PROCEDURE IF EXISTS user_search;
+DELIMITER //
+CREATE PROCEDURE `user_search`
+(
+	 pKeyword nvarchar(100)
+)
+BEGIN
+SELECT `users`.user_id,`users`.first_name, `users`.last_name, `users`.email   FROM `users`
+WHERE (`users`.first_name LIKE ('%',pKeyword,'%')) OR (CONCAT_WS(' ',`users`.first_name, `users`.last_name) LIKE ('%',pKeyword,'%'));
+END //
+DELIMITER ;
