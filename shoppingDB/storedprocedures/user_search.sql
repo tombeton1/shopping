fee0c5b566a4ea89090c80ca80416c11971170d3
Lenny <lenny.donnez@gmail.com>
2015-04-30 15:56:36 +0200

    user_search SP

--- shoppingDB/storedprocedures/user_search.sql
+++ shoppingDB/storedprocedures/user_search.sql
@@ -3,10 +3,10 @@ DROP PROCEDURE IF EXISTS user_search;
 DELIMITER //
 CREATE PROCEDURE `user_search`
 (
-	 pKeyword nvarchar(100)
+	 pKeyword varchar(100)
 )
 BEGIN
 SELECT `users`.user_id,`users`.first_name, `users`.last_name, `users`.email   FROM `users`
-WHERE (`users`.first_name LIKE ('%',pKeyword,'%')) OR (CONCAT_WS(' ',`users`.first_name, `users`.last_name) LIKE ('%',pKeyword,'%'));
+WHERE (`users`.first_name LIKE pKeyword) OR (CONCAT_WS(' ',`users`.first_name, `users`.last_name) LIKE pKeyword);
 END //
 DELIMITER ;
