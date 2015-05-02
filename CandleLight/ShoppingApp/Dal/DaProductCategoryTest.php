9f2337972b9e0a790b7de808a46080f3153e9221
Tom Adriaens <tomadriaens@yahoo.com>
2015-05-02 14:31:58 +0200

    DaProductCategory aan t testen werkt nog niet

---
+++ CandleLight/ShoppingApp/Dal/DaProductCategoryTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom
+ * Date: 2/05/2015
+ * Time: 14:15
+ */
+
+namespace ShoppingApp\Dal;
+use ShoppingApp\Bo\ProductCategory as ProductCategory;
+
+include_once '../../vendor/autoload.php';
+
+
+//$productCategory = new ProductCategory();
+//$productCategory->setProductCategoryName('Eenvoudig avond maal');
+//$productCategory->setProductCategoryDescription('snel klaar');
+//echo DaProductCategory::insert($productCategory);
+
+//echo DaProductCategory::delete(1);
