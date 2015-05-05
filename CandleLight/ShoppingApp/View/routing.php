df6fdfb98b78b089a2bf4c296fbf7888bc1dbc53
Lenny <lenny.donnez@gmail.com>
2015-05-05 15:54:43 +0200

    routing terug toegevoegd

---
+++ CandleLight/ShoppingApp/View/routing.php
@@ -0,0 +1,15 @@
+<?php
+include_once '../../vendor/autoload.php';
+if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+        case 'login':
+            $controller = new \ShoppingApp\Controllers\Authentication();
+            $controller->setPassword($_POST['password']);
+            $controller->setEmail($_POST['email']);
+            $controller->login();
+        case 'logout':
+            $controller = new \ShoppingApp\Controllers\Authentication();
+            $controller->logout();
+    }
+}
+?>
