a0a2c7bb050872e9ed51b3838802e26ebbda4940
Tom Adriaens <tombeton@yahoo.com>
2015-05-12 23:12:48 +0200

    insert user werkt enkel geen message

--- CandleLight/ShoppingApp/Controllers/User.php
+++ CandleLight/ShoppingApp/Controllers/User.php
@@ -26,7 +26,6 @@ class User {
         return json_encode($this->user->update($User));
     }
     public function insertUser($User){
-        return json_encode($this->user->insert($User));
-//        $this->user->insert($User);
+        $this->user->insert($User);
     }
 }
--- CandleLight/templates/index.php
+++ CandleLight/templates/index.php
@@ -34,7 +34,9 @@ if (!isset($_SESSION['token'])) {
             <button type="submit" name="action" value="login" id="login-button">Login</button>
         </form>
         <form class="form" action="">
-           <input class="button" id="register-close" type="submit" onclick="$('#register').toggle(); disableInput();"  value="Register" />
+            <label><?= $message ?></label><br>
+            <input class="button" id="register-close" type="submit" onclick="$('#register').toggle(); disableInput();"
+                   value="Register"/>
         </form>
         <div id="register" style="display: none">
             <form class="form" method="POST" id="insert-user-form">
@@ -42,9 +44,9 @@ if (!isset($_SESSION['token'])) {
                 <input type="text" name="first-name" placeholder="First Name" required="true">
                 <input type="text" name="last-name" placeholder="Last Name" required="true">
                 <input type="text" name="country" placeholder="Country" required="false">
-                <input type="password" name="password" placeholder="Password" required="true">
-                <input type="password" placeholder="Confirm password" required="true">
-                <label><?= $message ?></label><br>
+                <input type="password" id="txtPass" name="password" placeholder="Password" required="true">
+                <input type="password" id="txtConfirmPass"  placeholder="Confirm password" required="true">
+                <div id="confirm"></div>
                 <button type="submit" name="action" value="register" id="register-button">Register</button>
             </form>
         </div>
@@ -54,31 +56,41 @@ if (!isset($_SESSION['token'])) {
 <script>
     $(document).ready(function () {
         $('#insert-user-form').submit(function (e) {
-
             var insertbtn = $('#register-button');
             $.ajax({
-                url: '/CandleLight/api/users/',
+                url: '/CandleLight/api/users',
                 type: 'post',
                 dataType: 'text',
                 cache: false,
                 async: true,
                 data: $('#insert-user-form').serialize()
             }).done(function (data) {
-                disableInput();
+                $("#insert-user-form").trigger('reset');
+                $('#register').toggle();
+                document.getElementById("register-close").value = "Register";
+
             })
             e.preventDefault();
         });
+        $(function() {
+            $("#txtConfirmPass").keyup(function() {
+                var password = $("#txtPass").val();
+                $("#confirm").html(password == $(this).val() ? "Passwords match." : "Passwords do not match!");
+            });
+
+        });
 
     });
-        function disableInput() {
-            if (document.getElementById("register-close").type === "submit") {
-                document.getElementById("register-close").type = "button";
-                document.getElementById("register-close").value = "Close";
-            } else if (document.getElementById("update").type === "button") {
-                document.getElementById("update").type = "submit";
-            }
+    function disableInput() {
+        if (document.getElementById("register-close").type === "submit") {
+            document.getElementById("register-close").type = "button";
+            document.getElementById("register-close").value = "Close";
+        } else if (document.getElementById("register-close").type === "button") {
+            document.getElementById("register-close").type = "submit";
         }
+    }
     ;
+
 </script>
 </body>
 </html>
