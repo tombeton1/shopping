24529ec0d318b07ba1486256d37f20ef1c9ba822
Oualid Yousfi <oualid.yousfi@gmail.com>
2015-06-07 18:20:30 +0200

    database

--- shoppingDB/database_small_scope.sql
+++ shoppingDB/database_small_scope.sql
@@ -41,6 +41,7 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
+INSERT INTO `friends` VALUES (3,4,0),(4,2,0),(5,2,0),(6,5,0),(7,5,1),(8,5,1),(8,7,0),(5,4,0);
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +56,7 @@ CREATE TABLE `shopping_list` (
   `shopping_list_name` varchar(50) NOT NULL,
   `shopping_list_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `owner_text` varchar(1000) DEFAULT NULL,
+  `owner_text` varchar(10000) DEFAULT NULL,
   `friends_text` varchar(1000) DEFAULT NULL,
   `shopping_list_created` varchar(50) NOT NULL,
   `shopping_list_due_date` date DEFAULT NULL,
@@ -67,8 +68,18 @@ CREATE TABLE `shopping_list` (
   KEY `shopping_list_updated_by_fkey_idx` (`last_updated_by`),
   CONSTRAINT `shopping_list_updated_by_fkey` FOREIGN KEY (`last_updated_by`) REFERENCES `users` (`user_id`),
   CONSTRAINT `shopping_list_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shopping_list`
+--
+
+LOCK TABLES `shopping_list` WRITE;
+/*!40000 ALTER TABLE `shopping_list` DISABLE KEYS */;
+INSERT INTO `shopping_list` VALUES ('bloemkool',2,2,'','extra informatie','2015-06-02 17:13:21','2015-04-28','2015-06-02 15:23:03',2,3),('inserttest',3,2,'',NULL,'2015-06-02 17:15:57','2015-04-15','2015-06-02 15:15:57',1,NULL),('inserttest',7,4,'',NULL,'2015-06-02 17:55:11','2015-04-15','2015-06-02 15:55:11',1,NULL),('inserttest',8,4,'',NULL,'2015-06-02 17:55:11','2015-04-15','2015-06-02 15:55:11',1,NULL),('bloemkool',10,5,'',NULL,'2015-06-02 17:55:40','2015-04-28','2015-06-07 13:09:36',1,5),('broccoli',11,5,'<p>- <label>test</label> <input type=\"checkbox\"> <input class=\"button-flat button-yellow\" id=\"delete-text-list\" type=\"button\" value=\"Delete\" /></p>',NULL,'2015-06-02 17:55:40','2015-04-28','2015-06-07 13:28:14',0,5),('inserttest',12,5,'',NULL,'2015-06-02 17:55:40','2015-04-15','2015-06-02 15:55:40',1,NULL),('citroen',14,7,'<p>- <label style=\"color: red\">test</label> (testienn) <input type=\"checkbox\"> <input class=\"button-flat button-yellow\" id=\"delete-text-list\" type=\"button\" value=\"Delete\" /></p><p>- <label style=\"color: red\">list</label> (testienn) <input type=\"checkbox\"> <input class=\"button-flat button-yellow\" id=\"delete-text-list\" type=\"button\" value=\"Delete\" /></p>',NULL,'2015-06-05 15:25:23','2015-06-05','2015-06-07 16:15:13',1,5);
+/*!40000 ALTER TABLE `shopping_list` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -86,7 +97,7 @@ CREATE TABLE `users` (
   `password` char(60) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +106,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'tom','adriaens','bla','mljksqfdq','mlqsdkqlm'),(3,'adrie','mlkqsd','qsddqsq','sqddsqdsq','qsddsqqsd'),(4,'test','test','belgie','oualid@lid.com','$2y$10$VwBucYI/ZqK69UTE181p8unJnchUGHfieEJ0TJ0XYxTUbOBx9OEGC');
+INSERT INTO `users` VALUES (2,'tom','adriaens','bla','mljksqfdq','mlqsdkqlm'),(3,'adrie','mlkqsd','qsddqsq','sqddsqdsq','qsddsqqsd'),(4,'test','test','belgie','oualid@lid.com','$2y$10$VwBucYI/ZqK69UTE181p8unJnchUGHfieEJ0TJ0XYxTUbOBx9OEGC'),(5,'testienn','testzazaaez','belgie','test@test.com','$2y$10$0R0UOsafmf72O2ImkKcpBejaVedO/hB7tCCehfFwlWkCU2kjKMjIe'),(6,'lenny','donnez','belgie','lenny@tester.com','$2y$10$XC2re1ld8fMTu2jlIO8gXuTkSZN6JK/ltFeV5nL27N1SYldlOpCE6'),(7,'mokske','hot','belgie','mokske@tester.com','$2y$10$s80zdv0Ha2QKuFLxbyoSF.PggvY98RB6mUporFbFUKRYJ7QciVemO'),(8,'bokske','hot','belgie','bokske@tester.com','$2y$10$AAkAdnzqhFzEgllJ.GczUuMwZAZXvhr3pWoUR/bmjv5J2GAMWs886');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +121,7 @@ UNLOCK TABLES;
 /*!50003 SET character_set_results = utf8 */ ;
 /*!50003 SET collation_connection  = utf8_general_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `shopping_list_delete`(
 	 pId INT 
@@ -138,8 +149,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `shopping_list_insert`(
 	OUT pId INT ,
 	IN pName NVARCHAR (50) ,
     IN pUserId INT,
-    IN pOwnerText NVARCHAR (1000),
-    IN pCreated NVARCHAR (50),
     IN pDueDate DATE,
     IN pAccess TINYINT
 )
@@ -148,7 +157,6 @@ INSERT INTO `shopping_list`
 	(
 		`shopping_list`.`shopping_list_name`,
 		`shopping_list`.`user_id`,
-        `shopping_list`.`owner_text`,
 		`shopping_list`.`shopping_list_created`,
         `shopping_list`.`shopping_list_due_date`,
         `shopping_list`.`access`
@@ -157,12 +165,39 @@ INSERT INTO `shopping_list`
 	(
 		pName,
         pUserId,
-        pOwnerText,
         NOW(),
         pDueDate,
         pAccess
 	);
 	SELECT LAST_INSERT_ID() INTO pId;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `shopping_list_insert_text` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `shopping_list_insert_text`(
+	pId INT,
+	pOwnerText NVARCHAR (10000),
+	pLastUpdatedBy INT
+)
+BEGIN
+UPDATE `shopping_list`
+	SET
+		`owner_text` = pOwnerText,
+        `shopping_list_updated` = NOW(),
+		`last_updated_by` = pLastUpdatedBy
+	WHERE `shopping_list`.`shopping_list_id` = pId;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -207,10 +242,24 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `shopping_list_select_by_user`(
 	pId INT
 )
 BEGIN
-SELECT `shopping_list`.`shopping_list_name`, `shopping_list`.`shopping_list_id`,
-	`shopping_list`.`last_updated_by`, `shopping_list`.`shopping_list_due_date`,
-    `shopping_list`.`shopping_list_updated`
-	FROM `shopping_list` WHERE `user_id` = pId
+SELECT DISTINCT `invitee`.`first_name`, `shopping_list`.`shopping_list_name`, 
+	`shopping_list`.`user_id`, `shopping_list`.`shopping_list_id`, 
+    `shopping_list`.`shopping_list_due_date`, `shopping_list`.`shopping_list_updated`, 
+    `shopping_list`.`access`, `shopping_list`.`last_updated_by` FROM `friends` 
+    INNER JOIN `users` AS `inviter` ON `friends`.`user_id_inviter` = `inviter`.`user_id`
+    INNER JOIN `users` AS `invitee` ON `invitee`.`user_id` = `friends`.`user_id_invitee`
+    INNER JOIN `shopping_list` ON `invitee`.`user_id` = `shopping_list`.`user_id`
+    WHERE (`user_id_inviter` = pId OR `user_id_invitee` = pId) AND `relation_accepted` = 1
+    union
+SELECT DISTINCT `inviter`.`first_name`, `shopping_list`.`shopping_list_name`, 
+	`shopping_list`.`user_id`, `shopping_list`.`shopping_list_id`, 
+    `shopping_list`.`shopping_list_due_date`, `shopping_list`.`shopping_list_updated`, 
+    `shopping_list`.`access`, `shopping_list`.`last_updated_by` FROM `friends` 
+    INNER JOIN `users` AS `inviter` ON `friends`.`user_id_inviter` = `inviter`.`user_id`
+    INNER JOIN `users` AS `invitee` ON `invitee`.`user_id` = `friends`.`user_id_invitee`
+    INNER JOIN `shopping_list` ON `inviter`.`user_id` = `shopping_list`.`user_id`
+    WHERE (`user_id_inviter` = pId OR `user_id_invitee` = pId) AND `relation_accepted` = 1
+    ORDER BY 6 DESC
 ;
 END ;;
 DELIMITER ;
@@ -254,7 +303,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `shopping_list_update`(
 	pId INT ,
 	pName NVARCHAR (50) ,
 	pUserId INT,
-    pOwnerText NVARCHAR (1000),
     pDueDate DATE, 
     pAccess TINYINT
 )
@@ -262,12 +310,38 @@ BEGIN
 UPDATE `shopping_list`
 	SET
 		`shopping_list_name` = pName,
-		`user_id` = pUserId,
-        `owner_text` = pOwnerText,
 		`shopping_list_due_date` = pDueDate,
 		`shopping_list_updated` = NOW(),
 		`access` = pAccess,
         `last_updated_by` = pUserId
+	WHERE `shopping_list`.`shopping_list_id` = pId;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `shopping_list_update_by_friend` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `shopping_list_update_by_friend`(
+	pId INT,
+	pFriendsText NVARCHAR (1000),
+	pLastUpdatedBy INT
+)
+BEGIN
+UPDATE `shopping_list`
+	SET
+		`friends_text` = pFriendsText,
+        `shopping_list_updated` = NOW(),
+		`last_updated_by` = pLastUpdatedBy
 	WHERE `shopping_list`.`shopping_list_id` = pId;
 END ;;
 DELIMITER ;
@@ -339,7 +413,7 @@ DELIMITER ;
 /*!50003 SET character_set_results = utf8 */ ;
 /*!50003 SET collation_connection  = utf8_general_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `user_check_password`(
 	 pEmail nvarchar (100) 
@@ -367,6 +441,29 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `user_delete`(
 )
 BEGIN
 DELETE FROM `users` WHERE `users`.`user_id` = pId;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `user_delete_friend` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_delete_friend`(
+	IN pUserId INT ,
+	IN pFriendId INT 
+)
+BEGIN
+DELETE FROM `friends`
+	WHERE (`friends`.`user_id_inviter` = pUserId OR `friends`. `user_id_invitee` = pUserId) AND (`friends`.`user_id_invitee` = pFriendId OR `friends`.`user_id_inviter` = pFriendId);
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -437,6 +534,28 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `user_search` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_search`(
+	 pKeyword varchar(100)
+)
+BEGIN
+SELECT `users`.user_id,`users`.first_name, `users`.last_name, `users`.email   FROM `users`
+WHERE (`users`.first_name LIKE pKeyword) OR (CONCAT_WS(' ',`users`.first_name, `users`.last_name) LIKE pKeyword);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `user_select_all` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -482,6 +601,35 @@ THEN F.user_id_invitee= pUserId
 END
 AND 
 F.relation_accepted ='1';
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `user_select_friends_requests` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_select_friends_requests`(
+	pUserId INT
+)
+BEGIN
+SELECT U.user_Id, U.email, U.first_name, U.Last_name
+FROM `users` as `U`, `friends` as `F`
+WHERE
+CASE
+WHEN F.user_id_inviter= U.user_id
+THEN F.user_id_invitee= pUserId
+END
+AND 
+F.relation_accepted ='0';
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -553,4 +701,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-12 15:42:23
+-- Dump completed on 2015-06-07 18:18:07
